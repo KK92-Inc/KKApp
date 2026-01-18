21091d8c30dda7ceb074e033c23a08432eaa9144
W2Wizard <main@w2wizard.dev>
2026-01-18 14:41:04 +0100

    Another parameter

--- apphost.cs
+++ apphost.cs
@@ -98,7 +98,7 @@ var frontend = builder.AddViteApp("frontend", "./App.Frontend")
     .WithEnvironment("PROTOCOL_HEADER", "x-forwarded-proto")
     .WithEnvironment("PORT_HEADER", "x-forwarded-port")
     .WithEnvironment("XFF_DEPTH", "1")
-    //.WithEnvironment("ORIGIN", builder.AddParameter("frontend-origin"))
+    .WithEnvironment("ORIGIN", builder.AddParameter("frontend-origin"))
     .WithEnvironment("ADDRESS_HEADER", "True-Client-IP")
     //TODO: Remove on Aspire 13.2: https://github.com/dotnet/aspire/issues/13686
     .WithAnnotation(new JavaScriptPackageManagerAnnotation("bun", runScriptCommand: "run", cacheMount: "/root/.bun")
